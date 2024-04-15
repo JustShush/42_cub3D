988d0fb7ad514b7ecf53dbb3eec6382fb9050064
Tiago-0liveira <tfgoliveira@gmail.com>
2024-04-15 15:23:12 +0100

    basic structure with (libft, mlx)

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tiagoliv <tiagoliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 13:48:41 by tiagoliv          #+#    #+#             */
-/*   Updated: 2024/04/15 14:21:01 by tiagoliv         ###   ########.fr       */
+/*   Updated: 2024/04/15 15:22:14 by tiagoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,15 @@
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-
 	void	*mlx;
 	void	*mlx_win;
 
+	(void)argc;
+	(void)argv;
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1620, 880, "Cub3D");
 	mlx_loop(mlx);
-
 	free(mlx);
 	free(mlx_win);
-
 	return (0);
 }
